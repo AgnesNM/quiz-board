1132518e3b25beb6a1220c68db20f8bc178417fd
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-23 20:57:06 +0300

    add header elements, bootstrap elements, style with css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,33 @@
 //Business Logic
 var correctAnswers = [".alert();","f!== 15","//jQuery function","numbers = []"];
-var pointsPerCorrectAns = 4;
+var correctPoints = 4;
 
 function percent(points){
   return "Your score is " + parseInt((points/16)*100) + "%";
-  
 }
+
+$(document)ready(function()){
+  $("form#quiz").submit(function(event){
+    $("#total").text('');
+    var score = 0;
+    var Ans1 = ($("input[type=radio][name=q1]:checked").val());
+    var Ans2 = ($("input[type=radio][name=q2]:checked").val());
+    var Ans3 = ($("input[type=radio][name=q3]:checked").val());
+    var Ans4 = ($("input[type=radio][name=q4]:checked").val());
+
+    if (Ans1 === answers[0]){
+      score += correctPoints;
+      }
+      if (Ans2 === answers[1]){
+        score += correctPoints;
+        }
+        if (Ans3 === answers[2]){
+          score += correctPoints;
+          }
+          if (Ans4 === answers[3]){
+            score += correctPoints;
+            }
+            $("#total").text(percent(score));
+  });
+  event.preventDefault();
+};
