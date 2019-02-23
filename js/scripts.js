d6ed361e0ba13c37e6770e9e9da5f3c4f324b5b6
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-23 22:05:50 +0300

    create function in js for adding and removing classes

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+      $(document).ready(function() {
+        $("button#green").click(function() {
+          $("body").removeClass();
+          $("body").addClass("green-background");
+        });
+
+        $("button#blue").click(function() {
+          $("body").removeClass();
+          $("body").addClass("blue-background");
+        });
+            });
