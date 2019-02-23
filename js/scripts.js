9b75ad97c5e749ddb69e229009946107a5f885aa
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-23 17:12:03 +0300

    re-arrange business logic to achieve user strories

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,8 @@
-$(document).ready(function() {
-  $("form#quiz").submit(function(event) {
+//Business Logic
+var correctAnswers = [".alert();","f!== 15","//jQuery function","numbers = []"];
+var pointsPerCorrectAns = 4;
 
-    var answers = ["q1Ans1", "q1Ans2", "q1Ans3", "q2Ans1", "q2Ans2", "q2Ans3", "q3Ans1", "q3Ans2", "q3Ans3", "q3Ans2", "q4Ans1", "q4Ans2", "q4Ans3",
-      "q5Ans1", "q5Ans2", "q5Ans3"
-    ];
-
-    answers.forEach(function(answer) {
-      var userInput = $("input#" + answer).val();
-      $("#" + answer).text(userInput);
-    });
-    event.PreventDefault();
-  });
-});
+function percent(points){
+  return "Your score is " + parseInt((points/16)*100) + "%";
+  
+}
