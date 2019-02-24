aad0b4457bd8fee1fbc0c42f8c025f33ba9f37d7
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-24 05:03:11 +0300

    use event.PreventDefault to ensure submission is not done at the wrong time

--- js/scripts1.js
+++ js/scripts1.js
@@ -1,13 +1,12 @@
 var add = function(q1, q2,q3,q4) {
 return q1 + q2 + q3 + q4;
 };
-var percentage = function((q1 + q2 + q3 + q4)/16)*100 {
+var percentage = function(q1,q2,q3,q4) {
 return ((q1 + q2 + q3 + q4)/16)*100;
 };
 
 $(document).ready(function() {
 $("form#quiz").submit(function(event) {
-event.preventDefault();
 
 var q1 = parseInt($("input:radio [name=q1]:checked").val());
 var q2 = parseInt($("input:radio [name=q2]:checked").val());
@@ -16,4 +15,5 @@ var q4 = parseInt($("input:radio [name=q4]:checked").val());
 var result = percentage((q1 + q2 + q3 + q4)/16)*100
 $("#total").text("Your score is " + result);
 });
+event.preventDefault();
 });
