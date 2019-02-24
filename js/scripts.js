4cb969c4fb6d876e390517a47d5885b54dd8b43f
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-24 07:06:36 +0300

    edit js function to display user score as a number, display is 'your score is 0%'

--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,34 @@
             });
 
             });
+
+
+            //Business Logic
+            var add = function(q1, q2,q3,q4) {
+            return q1 + q2 + q3 + q4;
+            };
+            var percentage = function(q1,q2,q3,q4) {
+            return ((q1 + q2 + q3 + q4)/16)*100;
+            };
+            //UI Logic
+            $(document).ready(function() {
+            $("form#quiz").submit(function(event) {
+            event.preventDefault();
+            var q1 = parseInt($("input:radio [name=q1]:checked").val());
+            var q2 = parseInt($("input:radio [name=q2]:checked").val();
+            var q3 = parseInt($("input:radio [name=q3]:checked").val());
+            var q4 = parseInt($("input:radio [name=q4]:checked").val());
+            var points = 0;
+            for (var index = 0; index <4; index ++) {
+              points = points + 4;
+            }
+            var answers = [".alert();","f!== 15", "//jQuery function","var numbers = []"];
+            if q1 ==== answers[0];
+
+
+
+            var result = percentage((q1 + q2 + q3 + q4)/16)*100
+            $("#total").text("Your score is " + result);
+            });
+
+            });
--- js/scripts1.js
+++ js/scripts1.js
@@ -9,12 +9,30 @@ return ((q1 + q2 + q3 + q4)/16)*100;
 $(document).ready(function() {
 $("form#quiz").submit(function(event) {
 event.preventDefault();
-var q1 = parseInt($("input:radio [name=q1]:checked").val());
-var q2 = parseInt($("input:radio [name=q2]:checked").val();
-var q3 = parseInt($("input:radio [name=q3]:checked").val());
-var q4 = parseInt($("input:radio [name=q4]:checked").val());
+var q1 = $("input:radio [name=q1]:checked").val();
+var q2 = $("input:radio [name=q2]:checked").val();
+var q3 = $("input:radio [name=q3]:checked").val();
+var q4 =$("input:radio [name=q4]:checked").val();
+var answers = [".alert();","f!== 15", "//jQuery function","var numbers = []"];
+var points = [];
+if (q1 === answers[0]){
+  points += 4
+}
+if (q2 === answers[1]) {
+   points += 4
+}
+if (q3 === answers[2]) {
+  points += 4
+}
+if (q4 === answers[3]) {
+   points += 4
+}
+function percentage(points) {
+return "Your score is " + parseInt((points / 60) * 100) + "%";
+}
+
 var result = percentage((q1 + q2 + q3 + q4)/16)*100
-$("#total").text("Your score is " + result);
+$('#total').text(percentage(points));
 });
 
 });
