8a55f62154cdadcdf6a843fc1200c75662f4cfc4
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-24 05:22:51 +0300

    edit js function to display user score, score in Nan

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,24 @@
           $("body").addClass("blue-background");
         });
             });
+
+            //Business Logic
+            var add = function(q1, q2,q3,q4) {
+            return q1 + q2 + q3 + q4;
+            };
+            var percentage = function(q1,q2,q3,q4) {
+            return ((q1 + q2 + q3 + q4)/16)*100;
+            };
+            //UI Logic
+            $(document).ready(function() {
+            $("form#quiz").submit(function(event) {
+            event.preventDefault();
+            var q1 = parseInt($("input:radio [name=q1]:checked").val());
+            var q2 = parseInt($("input:radio [name=q2]:checked").val();
+            var q3 = parseInt($("input:radio [name=q3]:checked").val());
+            var q4 = parseInt($("input:radio [name=q4]:checked").val());
+            var result = percentage((q1 + q2 + q3 + q4)/16)*100
+            $("#total").text("Your score is " + result);
+            });
+
+            });
