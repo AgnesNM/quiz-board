0d70d5f08467f265c13047f37f9bc0e5330cc49a
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-25 03:48:58 +0300

    achieve more use cases

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,47 @@
-
-
-
+//Business Logic
+var add = function(q1, q2, q3, q4) {
+  return q1 + q2 + q3 + q4;
+};
+var percentage = function(q1, q2, q3, q4) {
+  return ((q1 + q2 + q3 + q4) / 16) * 100;
+};
 //UI Logic
-      $(document).ready(function() {
-        $("button#green").click(function() {
-          $("body").removeClass();
-          $("body").addClass("green-background");
-        });
+$(document).ready(function() {
+      $("form#quiz").submit(function(event) {
+        event.preventDefault();
+        var q1 = $("input:radio [name=q1]:checked").val();
+        var q2 = $("input:radio [name=q2]:checked").val();
+        var q3 = $("input:radio [name=q3]:checked").val();
+        var q4 = $("input:radio [name=q4]:checked").val();
+        var score = 0;
+        var points = 0;
+        var answers = [".alert();", "f!== 15", "//jQuery function", "var numbers = []"];
+        if (q1 === answers[0]) {
+          score += points;
+        }
+        if (q2 === answers[1]) {
+          score += points;
+        }
+        if (q3 === answers[2]) {
+          score += points;
+        }
+        if (q4 === answers[3]) {
+          score += points;
+        }
 
-        $("button#blue").click(function() {
-          $("body").removeClass();
-          $("body").addClass("blue-background");
-        });
-            });
+        function percentage(score) {
+          return "Your score is " + parseInt((score / 16) * 100) + "%";
+        }
+        $('#total').text(percentage(score));
 
-            //Business Logic
-            var add = function(q1, q2,q3,q4) {
-            return q1 + q2 + q3 + q4;
-            };
-            var percentage = function(q1,q2,q3,q4) {
-            return ((q1 + q2 + q3 + q4)/16)*100;
-            };
-            //UI Logic
-            $(document).ready(function() {
-            $("form#quiz").submit(function(event) {
-            event.preventDefault();
-            var q1 = parseInt($("input:radio [name=q1]:checked").val());
-            var q2 = parseInt($("input:radio [name=q2]:checked").val();
-            var q3 = parseInt($("input:radio [name=q3]:checked").val());
-            var q4 = parseInt($("input:radio [name=q4]:checked").val());
-            var result = percentage((q1 + q2 + q3 + q4)/16)*100
-            $("#total").text("Your score is " + result);
-            });
+          });
+          $("button#green").click(function() {
+            $("body").removeClass();
+            $("body").addClass("green-background");
+          });
 
-            });
-
-
-            //Business Logic
-            var add = function(q1, q2,q3,q4) {
-            return q1 + q2 + q3 + q4;
-            };
-            var percentage = function(q1,q2,q3,q4) {
-            return ((q1 + q2 + q3 + q4)/16)*100;
-            };
-            //UI Logic
-            $(document).ready(function() {
-            $("form#quiz").submit(function(event) {
-            event.preventDefault();
-            var q1 = parseInt($("input:radio [name=q1]:checked").val());
-            var q2 = parseInt($("input:radio [name=q2]:checked").val();
-            var q3 = parseInt($("input:radio [name=q3]:checked").val());
-            var q4 = parseInt($("input:radio [name=q4]:checked").val());
-            var points = 0;
-            for (var index = 0; index <4; index ++) {
-              points = points + 4;
-            }
-            var answers = [".alert();","f!== 15", "//jQuery function","var numbers = []"];
-            if q1 ==== answers[0];
-
-
-
-            var result = percentage((q1 + q2 + q3 + q4)/16)*100
-            $("#total").text("Your score is " + result);
-            });
-
-            });
+          $("button#blue").click(function() {
+            $("body").removeClass();
+            $("body").addClass("blue-background");
+          });
+      });
