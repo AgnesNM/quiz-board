a097f2d3fbed8c23c614d54ef8dc3c0b6ddd19b7
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-25 10:03:04 +0300

    edit radio button space so as to collect value

--- js/scripts.js
+++ js/scripts.js
@@ -9,31 +9,28 @@ var percentage = function(q1, q2, q3, q4) {
 $(document).ready(function() {
       $("form#quiz").submit(function(event) {
         event.preventDefault();
-        var q1 = $("input:radio [name=q1]:checked").val();
-        var q2 = $("input:radio [name=q2]:checked").val();
-        var q3 = $("input:radio [name=q3]:checked").val();
-        var q4 = $("input:radio [name=q4]:checked").val();
+        var q1 = $("input:radio[name=q1]:checked").val();
+        var q2 = $("input:radio[name=q2]:checked").val();
+        var q3 = $("input:radio[name=q3]:checked").val();
+        var q4 = $("input:radio[name=q4]:checked").val();
         var score = 0;
-        var points = 0;
+        var points = 4;
         var answers = [".alert();", "f!== 15", "//jQuery function", "var numbers = []"];
         if (q1 === answers[0]) {
           score += points;
         }
-        else if (q2 === answers[1]) {
+        if (q2 === answers[1]) {
           score += points;
         }
-        else if (q3 === answers[2]) {
+        if (q3 === answers[2]) {
           score += points;
         }
-        else if (q4 === answers[3]) {
+        if (q4 === answers[3]) {
           score += points;
         } else {
 
         }
-
-        function percentage(score) {
-          return "Your score is " + parseInt((score / 16) * 100) + "%";
-        }
+        
         $('#total').text(percentage(score));
 
           });
