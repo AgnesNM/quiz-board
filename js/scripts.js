49faccd5a2e735c12f2f602d469c7b9772531d8e
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-25 10:15:40 +0300

    update js to include all user stories

--- js/scripts.js
+++ js/scripts.js
@@ -7,40 +7,40 @@ var percentage = function(q1, q2, q3, q4) {
 };
 //UI Logic
 $(document).ready(function() {
-      $("form#quiz").submit(function(event) {
-        event.preventDefault();
-        var q1 = $("input:radio[name=q1]:checked").val();
-        var q2 = $("input:radio[name=q2]:checked").val();
-        var q3 = $("input:radio[name=q3]:checked").val();
-        var q4 = $("input:radio[name=q4]:checked").val();
-        var score = 0;
-        var points = 4;
-        var answers = [".alert();", "f!== 15", "//jQuery function", "var numbers = []"];
-        if (q1 === answers[0]) {
-          score += points;
-        }
-        if (q2 === answers[1]) {
-          score += points;
-        }
-        if (q3 === answers[2]) {
-          score += points;
-        }
-        if (q4 === answers[3]) {
-          score += points;
-        } else {
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    var q1 = $("input:radio[name=q1]:checked").val();
+    var q2 = $("input:radio[name=q2]:checked").val();
+    var q3 = $("input:radio[name=q3]:checked").val();
+    var q4 = $("input:radio[name=q4]:checked").val();
+    var score = 0;
+    var points = 4;
+    var answers = [".alert();", "f!== 15", "//jQuery function", "var numbers = []"];
+    if (q1 === answers[0]) {
+      score += points;
+    } if (q2 === answers[1]) {
+      score += points;
+    } if (q3 === answers[2]) {
+      score += points;
+    } if (q4 === answers[3]) {
+      score += points;
+    } else {
 
-        }
-        
-        $('#total').text(percentage(score));
+    }
 
-          });
-          $("button#green").click(function() {
-            $("body").removeClass();
-            $("body").addClass("green-background");
-          });
+    function percentage(score) {
+      return "Your score is " + parseInt((score / 16) * 100) + "%";
+    }
+    $('#total').text(percentage(score));
 
-          $("button#blue").click(function() {
-            $("body").removeClass();
-            $("body").addClass("blue-background");
-          });
-      });
+  });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+});
